2c64a6be554bf6d0a87ff85dccff461f05abda40
Julia Peres Kitzberger <xjujuperesx@hotmail.com>
2023-02-28 15:12:37 -0300

    Criação do middleware validação do usuário

--- back-end/src/routes/users.routes.js
+++ back-end/src/routes/users.routes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const userController = require('../controllers/users.controller');
 
+
 const router = express.Router();
 
 router.post('/', userController.userLogin);
