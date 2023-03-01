fce12508631c96e802a7bef3f3636c3db14acd39
opabloaraujo <opabloaraujo@protonmail.com>
2023-03-01 18:23:14 -0300

    Atualizando o ORM Products

--- back-end/src/routes/products.routes.js
+++ back-end/src/routes/products.routes.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const ProductController = require('../controllers/products.controller');
+
 const router = express.Router();
 
 router.get('/', ProductController.getAllProducts);
