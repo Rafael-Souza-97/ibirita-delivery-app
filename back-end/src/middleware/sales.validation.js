9322608f25335c4b5750d6ee572a561b812b6dae
Julia Peres Kitzberger <xjujuperesx@hotmail.com>
2023-03-03 17:47:36 -0300

    Correção do middleware

--- back-end/src/middleware/sales.validation.js
+++ back-end/src/middleware/sales.validation.js
@@ -1,13 +1,10 @@
 const salesValidation = async(req, res, next) => {
-  const { sellerId, totalPrice, deliveryAdress, deliveryNumber } = req.body; 
+  const { sellerId, totalPrice, deliveryAddress, deliveryNumber } = req.body; 
   
-  if (!sellerId || !totalPrice || !deliveryAdress || !deliveryNumber) {
+  if (!sellerId || !totalPrice || !deliveryAddress || !deliveryNumber) {
     return res.status(404).json({ message: 'Missing fields required' });
   }
-  if (deliveryNumber !== Number) {
-    return res.status(400).json({ message: 'Delivery number must be a number' });
-  }
-  if (totalPrice !== Number) {
+  if (typeof totalPrice !== 'number') {
     return res.status(400).json({ message: 'Total price must be a number'});
   }
   next();
