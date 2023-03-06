ef522ef150be9402b7b8a82ad0258a9c38116614
pedrohassen <pedro_hasse@yahoo.com.br>
2023-03-06 15:34:49 -0300

    refactor: 💡 Refatora o update status da sales
    Integra vários endpoints em um só

--- back-end/src/utils/error.messages.js
+++ back-end/src/utils/error.messages.js
@@ -2,10 +2,13 @@ const SALES_NOT_FOUND = 'No sales found';
 const SALES_NOT_FOUND_ERROR = new Error(SALES_NOT_FOUND);
 const SALE_NOT_FOUND = 'Sale not found';
 const SALE_NOT_FOUND_ERROR = new Error(SALE_NOT_FOUND);
+const STATUS_REQUIRED = 'Status is required';
+const STATUS_REQUIRED_ERROR = new Error(STATUS_REQUIRED);
 
 module.exports = {
   SALES_NOT_FOUND,
   SALE_NOT_FOUND,
   SALES_NOT_FOUND_ERROR,
   SALE_NOT_FOUND_ERROR,
+  STATUS_REQUIRED_ERROR,
 };
