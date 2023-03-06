5f384814136e514919f5f28c8e426f97fa0c5c0d
Artur <vidorartur@gmail.com>
2023-03-06 14:14:18 -0300

    fazendo pull

--- front-end/src/components/NavBar.jsx
+++ front-end/src/components/NavBar.jsx
@@ -1,32 +1,39 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { useHistory } from 'react-router-dom';
+import Context from '../context/context';
+import '../styles/NavBar.css';
 
 export default function NavBar() {
+  const { setIsLoged } = useContext(Context);
   const userJson = localStorage.getItem('user');
   const data = JSON.parse(userJson);
   const history = useHistory();
 
   const logout = () => {
+    setIsLoged(false);
     localStorage.clear();
     history.push('/login');
   };
 
   return (
-    <div>
-      <nav>
-        <ul>
+    <div className="header">
+      <nav className="navbar-left">
+        <ul className="nav-list-left">
           <li
+            className="nav-item"
             data-testid="customer_products__element-navbar-link-products"
           >
             <button
+              className="nav-button"
               type="button"
               onClick={ () => history.push('/customer/products') }
             >
               Produtos
             </button>
           </li>
-          <li>
+          <li className="nav-item">
             <button
+              className="nav-button"
               data-testid="customer_products__element-navbar-link-orders"
               type="button"
               onClick={ () => history.push('/customer/orders') }
@@ -34,13 +41,20 @@ export default function NavBar() {
               Meus Pedidos
             </button>
           </li>
+        </ul>
+      </nav>
+
+      <nav className="navbar-right">
+        <ul className="nav-list-right">
           <li
+            className="nav-item"
             data-testid="customer_products__element-navbar-user-full-name"
           >
             {data.name}
           </li>
-          <li>
+          <li className="nav-item">
             <button
+              className="nav-button"
               data-testid="customer_products__element-navbar-link-logout"
               type="button"
               onClick={ logout }
@@ -51,5 +65,6 @@ export default function NavBar() {
         </ul>
       </nav>
     </div>
+
   );
 }
