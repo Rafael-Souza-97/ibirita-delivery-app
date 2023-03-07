993715def2cfa5a22c8538ddd578c99588d48e4a
pedrohassen <pedro_hasse@yahoo.com.br>
2023-03-07 18:09:34 -0300

    refactor: 💡 Refatora o retorno do deleteUserById

--- back-end/src/controllers/users.controller.js
+++ back-end/src/controllers/users.controller.js
@@ -94,8 +94,8 @@ const deleteUserById = async (req, res) => {
   const { id } = req.params;
 
   try {
-    const user = await usersService.deleteUserById(id);
-    return res.status(200).json(user);
+    await usersService.deleteUserById(id);
+    return res.status(200).json({ message: 'User deleted' });
   } catch (error) {
     return res.status(404).json({ message: error.message });
   }
