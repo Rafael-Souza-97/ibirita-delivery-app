893ef75dd20ea76aa3437189e4d0af4706fd2cea
pedrohassen <pedro_hasse@yahoo.com.br>
2023-03-07 16:53:23 -0300

    feat: 🦾 Função getAllUsers
    Cria a função que retorna todos os usuários, cria validações de token
    para respectivas roles

--- back-end/src/utils/error.messages.js
+++ back-end/src/utils/error.messages.js
@@ -4,6 +4,8 @@ const SALE_NOT_FOUND = 'Sale not found';
 const SALE_NOT_FOUND_ERROR = new Error(SALE_NOT_FOUND);
 const STATUS_REQUIRED = 'Status is required';
 const STATUS_REQUIRED_ERROR = new Error(STATUS_REQUIRED);
+const TOKEN_NOT_FOUND = { message: 'Token not found' };
+const EXPIRED_INVALID_TOKEN = { message: 'Expired or invalid token' };
 
 module.exports = {
   SALES_NOT_FOUND,
@@ -11,4 +13,6 @@ module.exports = {
   SALES_NOT_FOUND_ERROR,
   SALE_NOT_FOUND_ERROR,
   STATUS_REQUIRED_ERROR,
+  TOKEN_NOT_FOUND,
+  EXPIRED_INVALID_TOKEN,
 };
